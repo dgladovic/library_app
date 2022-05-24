b4bd87ba9e5fd278c6d55c4fd444563a3b967671
Dimitrije <rdomiran@gmail.com>
2022-05-24 16:57:59 +0200

    funkcije dodavanja knjiga rade

---
+++ myscript.js
@@ -0,0 +1,79 @@
+let myLibrary = [
+{title: "mala nocna muzika",
+ author: "mozzart",
+ pages: 25,
+ read: "jesam"},
+ {title: "pederi"
+
+ }
+
+];
+
+const tijelo = document.querySelector('body');
+
+function Book(title,author,pages,read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(){
+
+  let naslov = document.querySelector('#title');
+  let autor = document.querySelector('#author');
+  let broj = document.querySelector('#pages');
+  let citanje = document.querySelector('input[name="read"]:checked');
+  let djokic = new Book(naslov.value,autor.value,broj.value,citanje.value);
+
+  console.log(myLibrary);
+  return myLibrary.push(djokic);
+
+    
+}
+
+function carousell(){
+    myLibrary.forEach( (book) => {
+        const kartica = document.createElement('div');
+        kartica.style.width = '400px';
+        for (key in book){
+            const title = document.createElement('p');
+            title.style.width = '60px';
+            title.style.display = "inline-block"
+
+            const tekst = document.createElement('p');
+            tekst.style.fontSize = '18px';
+            tekst.style.margin = '2px';
+            tekst.style.display = "inline-block"
+
+
+            title.style.fontSize = '18px';
+            title.style.margin = '2px';
+
+
+            switch(key){
+                case 'title': 
+                    title.textContent = "Title: ";
+                    tekst.textContent = book[key];
+                    break;
+                case 'author':
+                    title.textContent = "Author: ";
+                    tekst.textContent = book[key];
+                    break;
+                case 'pages':
+                    title.textContent = "Pages: ";
+                    tekst.textContent = book[key];
+                    break;
+                case 'read':
+                    title.textContent = "Status: ";
+                    tekst.textContent = book[key];
+                    break;
+            }
+
+            kartica.appendChild(title);
+            kartica.appendChild(tekst);
+        }
+        tijelo.appendChild(kartica);
+    })
+}
+
